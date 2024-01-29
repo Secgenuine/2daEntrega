4bb679b8cffe46806584a9b42d49ff40d68fb242
Secgenuine <151797055+Secgenuine@users.noreply.github.com>
2024-01-29 20:13:37 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let usuarios=[
+    {
+        nombre:"maxi",
+        monto:"120000",
+        cuotas:6,
+        tasa:120
+    },
+    {
+        nombre:"debora",
+        monto:"900000",
+        cuotas:120,
+        tasa:80
+    },
+    {
+        nombre:"belu",
+        monto:"500000",
+        cuotas:3,
+        tasa:140
+    }
+
+]
+
+console.log(usuarios);
+
+function usuario(nombre, monto, cuotas, tasa){
+
+this.nombre=(prompt("Ingrese su nombre").toLowerCase());
+this.monto=(prompt("ingrese el monto del bien a comprar").toLowerCase());
+this.cuotas=(prompt("ingrese cantidad de cuotas").parseInt);
+this.tasa=(prompt("ingrese la tasa de interés anual").parseFloat);
+
+usuario (NOMBRE, MONTO, CUOTAS, TASA);
+}
